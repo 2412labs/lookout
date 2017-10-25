fa4c022910ae54ec54718037f8dbc88f6f309c25
Ben <Ben@Benjis-MacBook-Air.local>
2017-10-25 13:39:46 -0400

    update to python3

--- device/camera/example/run.sh
+++ device/camera/example/run.sh
@@ -2,4 +2,4 @@
 : ${1?must provide path to log directory}
 cd "$(dirname "$0")"
 mkdir -p $1
-python -u cameraThingExample.py -c conf.json 2>&1 | svlogd -tt $1 
+python3 -u cameraThingExample.py -c conf.json 2>&1 | svlogd -tt $1 
